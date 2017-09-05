a2b1f34a2c7e57454a37e3b81064556d9a64a2dd
Albert David <albert.david@gmail.com>
2017-09-05 17:23:32 +0200

    openwrt: fixed build-openwrt-a5v11.sh for http proxy error

--- build-openwrt-a5v11.sh
+++ build-openwrt-a5v11.sh
@@ -5,4 +5,7 @@ sh -x ./setup
 cp ../../mka5v11 openwrt/
 cp ../../mkonce openwrt/
 cp ../../config_a5-v11 openwrt/configfiles/A5-V11/.config_a5-v11
+export http_proxy=
+export https_proxy=
+export ftp_proxy=
 ./openwrt/mkonce
