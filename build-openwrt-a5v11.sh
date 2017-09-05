8fec81e88dd0861637f1f70d158369b2e7e5bba7
Albert David <albert.david@gmail.com>
2017-09-05 15:48:55 +0200

    openwrt: added build setup preperation files

---
+++ build-openwrt-a5v11.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+unzip -d openwrt goldenorb.zip
+cd openwrt/chaoscalmer/
+sh -x ./setup
+cp ../../mka5v11 openwrt/
+cp ../../mkonce openwrt/
+cp ../../config_a5-v11 openwrt/configfiles/A5-V11/.config_a5-v11
+./openwrt/mkonce
